3e3e16ba6c5469c5f7b671ef7dedcc06b91eff01
zdxxfghj <dolganovvlad486@gmail.com>
2024-05-16 15:04:59 +0300

    first commit

--- FeedBackProject/settings.py
+++ FeedBackProject/settings.py
@@ -104,6 +104,15 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'dolganov045@gmail.com'
+EMAIL_HOST_PASSWORD = 'byvi dzsj zzii mnse'
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
+ALLOWED_HOSTS = []
+
 
 
 LANGUAGE_CODE = 'ru-RU'
--- feedback/serializers.py
+++ feedback/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from .models import CarModel, FeedBack
-
+from django.core.mail import send_mail
 
 class CarModelSerializer(serializers.ModelSerializer):
     class Meta:
@@ -16,6 +16,21 @@ class FeedBackSerializer(serializers.ModelSerializer):
     def save(self):
         feedback = FeedBack.objects.create(**self.validated_data)
         email = self.validated_data['email']
-        # send_email()
+        car = self.validated_data['car']
+        damage_description = self.validated_data['damage_description']
+        contact_info = self.validated_data['contact_info']
+        year = self.validated_data['year']
+
+        message = "Пользователь с email: "+ email +"\nКонтактные данные:"+ contact_info + "\nАвтомобиль:"+ str(car)+ "\nГод авто:" + str(year) + "\nОписание повреждения и расчет стоимости:" + damage_description
+
+        send_mail("Заявка отправлена в обработку. @Atlant M", "С вами свзяжется менеджер!", 'dolganov045@gmail.com', [email], fail_silently=False)
+        send_mail("Заполнена форма обратной связи Кузовного ремонта. @Atlant M", message, 'dolganov045@gmail.com', ['stefaniya11092002@gmail.com'],fail_silently=False)
+
+
+        print(damage_description)
         return feedback
+
+
+
+
 
